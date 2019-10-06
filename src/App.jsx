491cb224df8b4e26d9aa60bb50ea3818790369af
Kashif <kashif.dev@protonmail.com>
2019-10-06 12:03:17 +0530

    initial

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import React, { Component } from 'react'
+
+export default class App extends Component {
+
+  state = {
+
+  }
+
+  render() {
+    return (
+      <div className='app' >
+        This my app.
+      </div>
+    )
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
+
+const CSSPlugin = new MiniCSSExtractPlugin({
+  filename: 'style.[contenthash].css'
+})
+
+const HTMLPlugin = new HTMLWebpackPlugin({
+  template: './src/index.html',
+  filename: 'index.html'
+})
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, '/build'),
+    filename: '[name].[chunkhash].js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /.js(x)?$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /.s*[ac]ss$/,
+        use: ['style-loader', MiniCSSExtractPlugin.loader, 'css-loader', 'sass-loader']
+      },
+      {
+        test: /.(ttf|otf|svg)/,
+        use: 'file-loader'
+      }
+    ]
+  },
+  plugins: [CSSPlugin, HTMLPlugin],
+  devServer: {
+    proxy: [{
+      context: ['/user'],
+      target: 'http://localhost:5000'
+    }],
+    historyApiFallback: true
+  }
+}
